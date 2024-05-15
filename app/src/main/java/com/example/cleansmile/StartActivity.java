8044151c82d5e0ce81349366d9f0ba0093406303
Laura-Michelle Hahn <laura-michelle.hahn@edu.fh-joanneum.at>
2024-05-15 10:02:39 +0200

    Zoom

--- app/src/main/java/com/example/cleansmile/StartActivity.java
+++ app/src/main/java/com/example/cleansmile/StartActivity.java
@@ -17,18 +17,44 @@ public class StartActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.Button);
         SeekBar zoom = findViewById(R.id.zoom);
-        EditText user = findViewById(R.id.User);
+        EditText username = findViewById(R.id.User);
         EditText password = findViewById(R.id.Password);
         Button newUser = findViewById(R.id.newUser);
 
-        String a = user.getText().toString();
-        String b = password.getText().toString();
+        String user = username.getText().toString();
+        String passWord = password.getText().toString();
+
+
         //DB anbindung erforderlich (ROOM)
         //Bei Clicken von button -> prüfen ob Daten korrekt sind
         //Bei newUser -> prüfen ob Daten noch nicht vorhanden sind
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+            }
+        });
+        //Neuen User anlegen
+        newUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        // Video: https://www.youtube.com/watch?v=dJvpDtFrk0A
+        zoom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBar.setText(progress+"/"+"50");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
         });
