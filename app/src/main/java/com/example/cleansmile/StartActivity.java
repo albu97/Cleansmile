0b07670a4474063356eb4003294da421b48d3a44
Laura-Michelle Hahn <laura-michelle.hahn@edu.fh-joanneum.at>
2024-05-15 09:34:17 +0200

    Password, User

--- app/src/main/java/com/example/cleansmile/StartActivity.java
+++ app/src/main/java/com/example/cleansmile/StartActivity.java
@@ -3,11 +3,12 @@ package com.example.cleansmile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,23 @@ public class MainActivity extends AppCompatActivity {
         SeekBar zoom = findViewById(R.id.zoom);
         EditText user = findViewById(R.id.User);
         EditText password = findViewById(R.id.Password);
+        Button newUser = findViewById(R.id.newUser);
+
+        String a = user.getText().toString();
+        String b = password.getText().toString();
+        //DB anbindung erforderlich (ROOM)
+        //Bei Clicken von button -> prüfen ob Daten korrekt sind
+        //Bei newUser -> prüfen ob Daten noch nicht vorhanden sind
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
     }
     //TESt
 
