922e31a53c2c797daa225bff955b817f65102383
Laura-Michelle Hahn <laura-michelle.hahn@edu.fh-joanneum.at>
2024-05-15 08:53:57 +0200

    Password, User

--- app/src/main/java/com/example/cleansmile/MainActivity.java
+++ app/src/main/java/com/example/cleansmile/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.cleansmile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.Button);
+        SeekBar zoom = findViewById(R.id.zoom);
+        EditText user = findViewById(R.id.User);
+        EditText password = findViewById(R.id.Password);
     }
     //TESt
 
